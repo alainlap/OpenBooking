37a2c9cb1f9692d8b3d0359add91ce9cf8a57a94
alainlap <alainlaplume@gmail.com>
2013-11-15 16:47:11 -0500

    set up user model with sorcery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenBooking::Application.config.secret_key_base = 'cf16d51b563d756f64b297217caed8b7e94f5284d3639fda3d2d5c53d8145cdd431842484416367ec0574f1c347bc8d90b788e918b5ea047537d8566fc73c98d'
