a53f98b86553d73d7f0598c15c2d6ebc1982f6b2
Katrina Verey <kn.verey@gmail.com>
2013-11-19 18:42:15 -0500

    Added validation for duplicate provider bookings.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,20 +4,28 @@ class Appointment < ActiveRecord::Base
 	validates_presence_of :start_datetime, :end_datetime, :reason
 
 	validate :slot_available?
+	# validate :duplicate_client_booking?
+	validate :duplicate_provider_booking?
 
 	def slot_available?
-    req_start = self.start_datetime
-    req_end = self.end_datetime
-
-    availability_on_day = self.provider.availabilities.find_by_day(req_start.wday)
+    availability_on_day = self.provider.availabilities.find_by_day(start_datetime.wday)
 
     if availability_on_day.nil?
     	errors.add(:start_datetime, "#{self.provider.name} is not available on that day.") 
     else
-	    errors.add(:start_datetime, "Your appointment starts too early.") if req_start.hour < availability_on_day.start_time
-	 		errors.add(:start_datetime, "Your appointment starts too late.") if req_start.hour >=availability_on_day.end_time
-	    errors.add(:end_datetime, "Your appointment cannot end before it starts!") if req_end.hour < req_start.hour
-	    errors.add(:end_datetime, "Your appointment runs too late.") if req_end.hour > availability_on_day.end_time
+	    errors.add(:start_datetime, "Your appointment starts too early.") if start_datetime.hour < availability_on_day.start_time
+	 		errors.add(:start_datetime, "Your appointment starts too late.") if start_datetime.hour >=availability_on_day.end_time
+	    errors.add(:end_datetime, "Your appointment cannot end before it starts!") if end_datetime.hour < start_datetime.hour
+	    errors.add(:end_datetime, "Your appointment runs too late.") if end_datetime.hour > availability_on_day.end_time
 	  end
   end
+
+  def duplicate_provider_booking?
+  	errors.add(:end_datetime, "Your end time overlaps an existing appointment.") unless self.provider.appointments.where("? > start_datetime AND ? <= end_datetime", end_datetime, end_datetime).empty?
+
+  	errors.add(:start_datetime, "Your start time overlaps an existing appointment.") unless self.provider.appointments.where("? >= start_datetime AND ? < end_datetime", start_datetime, start_datetime).empty?
+
+  	errors.add(:start_datetime, "Your appointment completely overlaps an existing appointment.") unless self.provider.appointments.where("? <= start_datetime AND ? >= end_datetime", start_datetime, end_datetime).empty?
+  end
 end
+
