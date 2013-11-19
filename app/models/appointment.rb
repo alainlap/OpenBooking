b7e5354acdd75eeca3023fcd71b43c35e13e778f
Katrina Verey <kn.verey@gmail.com>
2013-11-19 18:53:53 -0500

    Added validation for appointments against duplicate client booking

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment < ActiveRecord::Base
 	validates_presence_of :start_datetime, :end_datetime, :reason
 
 	validate :slot_available?
-	# validate :duplicate_client_booking?
+	validate :duplicate_client_booking?
 	validate :duplicate_provider_booking?
 
 	def slot_available?
@@ -26,6 +26,14 @@ class Appointment < ActiveRecord::Base
   	errors.add(:start_datetime, "Your start time overlaps an existing appointment.") unless self.provider.appointments.where("? >= start_datetime AND ? < end_datetime", start_datetime, start_datetime).empty?
 
   	errors.add(:start_datetime, "Your appointment completely overlaps an existing appointment.") unless self.provider.appointments.where("? <= start_datetime AND ? >= end_datetime", start_datetime, end_datetime).empty?
+  end
+
+   def duplicate_client_booking?
+  	return errors.add(:end_datetime, "You have another appointment at this time.") unless self.client.appointments.where("? > start_datetime AND ? <= end_datetime", end_datetime, end_datetime).empty?
+
+  	return errors.add(:start_datetime, "You have another appointment at this time.") unless self.client.appointments.where("? >= start_datetime AND ? < end_datetime", start_datetime, start_datetime).empty?
+
+  	errors.add(:start_datetime, "You have another appointment at this time.") unless self.client.appointments.where("? <= start_datetime AND ? >= end_datetime", start_datetime, end_datetime).empty?
   end
 end
 
