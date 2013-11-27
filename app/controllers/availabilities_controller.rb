a93fcc096df8cdfc7919857fb1d55aec6b8a1c98
alainlap <alainlaplume@gmail.com>
2013-11-27 17:35:55 -0500

    converted availabilities index to AJAX and form to form helper

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -6,13 +6,22 @@ class AvailabilitiesController < ApplicationController
 	end
 
 	def update
-		@availability = Availability.find(availabilities_params[:id])
-		if @availability.update(availabilities_params)
-    	redirect_to user_provider_availabilities_path(@user, @provider), flash: {success: 'Availability successfully saved.'}
-  	else
-  		@availabilities = @provider.availabilities.order(:id)
-  	  flash[:alert] = "Please try again!"
-  	  render 'index'
+		@availability = Availability.find(params[:id])
+		@availabilities = @provider.availabilities.order(:id)
+
+		respond_to do |format|
+
+			if @availability.update(availabilities_params)
+
+	    	format.html {redirect_to user_provider_availabilities_path(@user, @provider), flash: {success: 'Availability successfully saved.'}}
+	    	format.js
+	  	else
+	  		format.html {			
+	  	  	flash[:alert] = "Please try again!"
+	  	  	render 'index'
+	  	  }
+	  	  format.js
+	  	end
   	end
 	end
 
@@ -23,7 +32,7 @@ class AvailabilitiesController < ApplicationController
 
 	private
 	def availabilities_params
-		params.permit(:day, :start_time, :end_time, :provider_id, :id)
+		params.require(:availability).permit(:day, :start_time, :end_time, :provider_id)
 	end
 
 	def set_provider
